37f909cea33d5a3c11904c67bd2a003d17f380e3
Yuri Cunha <54660107+isyuricunha@users.noreply.github.com>
2022-06-20 15:48:44 -0300

    Update lista1.jsx

--- exercicio/revisao react/pages/desafio/lista1.jsx
+++ exercicio/revisao react/pages/desafio/lista1.jsx
@@ -1,50 +1,50 @@
-const random = (length = 8) => {
-  return Math.random().toString(16).substr(2, length);
-};
+// const random = (length = 8) => {
+//   return [Math.random().toString(16).substr(2, length)];
+// };
 
-export default function lista1() {
-  return (
-    <div>
-      <span>
-        Aqui 01: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 02: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 03: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 05: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 04: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 06: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 07: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 08: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 09: {random()}
-        <br />
-      </span>
-      <span>
-        Aqui 10: {random()}
-        <br />
-      </span>
-    </div>
-  );
-}
+// export default function lista1() {
+//   return (
+//     <div>
+//       <span>
+//         Aqui 01: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 02: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 03: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 05: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 04: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 06: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 07: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 08: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 09: {random()}
+//         <br />
+//       </span>
+//       <span>
+//         Aqui 10: {random()}
+//         <br />
+//       </span>
+//     </div>
+//   );
+// }
