13f3af071bbb95cf0cabd93db317634c129598de
Yuri Cunha <54660107+isyuricunha@users.noreply.github.com>
2022-06-20 15:06:23 -0300

    Update jsx04.jsx

--- exercicio/revisao react/pages/basico/jsx04.jsx
+++ exercicio/revisao react/pages/basico/jsx04.jsx
@@ -1,11 +1,12 @@
 export default function jsx4() {
   const subtitulo = "Estou no JavaScript";
+  const trechoH3 = <h3>{3 * 3}</h3>;
 
   return (
     <div>
       <h1>Integração JS e JSX</h1>
       <h2>{subtitulo}</h2>
-      <h3>{3 * 3}</h3>
+      {trechoH3}
       <h4>{Math.max(1, 100)}</h4>
       <h5>{entre(5, 1, 10)}</h5>
     </div>
