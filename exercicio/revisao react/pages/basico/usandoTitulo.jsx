a347b9035d79ff2ddacd94d02d99438f62c3d219
Yuri Cunha <isyuricunha@icloud.com>
2022-06-25 11:31:37 -0300

    component titulo

---
+++ exercicio/revisao react/pages/basico/usandoTitulo.jsx
@@ -0,0 +1,13 @@
+import Titulo from "../../components/Titulo";
+
+export default function usandoTitulo() {
+  return (
+    <div>
+      <Titulo />
+    </div>
+  );
+}
+
+
+// posso importar um component para reuso, o que é bem mais confortavel
+// reultilizando o componente faz mais sentido do que refazer
