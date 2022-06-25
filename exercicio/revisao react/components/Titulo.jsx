1e3f6e9803774ecc61c331d97b666fa14c9b0891
Yuri Cunha <isyuricunha@icloud.com>
2022-06-25 12:05:38 -0300

    Propriedade Componente

--- exercicio/revisao react/components/Titulo.jsx
+++ exercicio/revisao react/components/Titulo.jsx
@@ -1,8 +1,17 @@
-export default function Titulo() {
-   return (
-      <>
-         <h1>Titulo</h1>
-         <h2>Subtitulo</h2>
-      </>
-   )
+export default function Titulo(props) {
+  //   console.log(props);
+  return props.pequeno ? (
+    <>
+      <p> {props.principal} </p>
+      <p> {props.secundario} </p>
+    </>
+  ) : (
+    <>
+      <h1> {props.principal} </h1>
+      <h1> {props.secundario} </h1>
+    </>
+  );
 }
+
+// ? se nao for, use este ...
+// : se for, use este
--- exercicio/revisao react/pages/basico/usandoTitulo.jsx
+++ exercicio/revisao react/pages/basico/usandoTitulo.jsx
@@ -3,10 +3,26 @@ import Titulo from "../../components/Titulo";
 export default function usandoTitulo() {
   return (
     <div>
-      <Titulo />
+      <Titulo
+        principal="Página de cadatro"
+        secundario="Incluir, alterar e excluir coisas!"
+      />
+      <Titulo
+        principal="Página de login"
+        secundario="Informe seu e-mail e senha"
+        pequeno
+      />
+      <Titulo
+        principal="Página de login"
+        secundario="Informe seu e-mail e senha"
+      />
     </div>
   );
 }
 
 // posso importar um component para reuso, o que é bem mais confortavel
 // reultilizando o componente faz mais sentido do que refazer
+
+// o true define que aquele é o verdadeiro, que, no caso, para ele usar como pequeno
+// mas nao precisa dizer explicitamente se ele é verdade
+// só é preciso passar o parametro
