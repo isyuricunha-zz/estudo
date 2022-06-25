e13e717f9ae1c3d32c53fd431a6d0e3a3ebbc670
Yuri Cunha <isyuricunha@icloud.com>
2022-06-25 12:40:48 -0300

    Integração com CSS #01

---
+++ exercicio/revisao react/pages/css/integracao01.jsx
@@ -0,0 +1,14 @@
+// nao posso importar aqui
+// o import foi feito de maneira global
+
+// o import do css global está na no _app.js
+
+export default function integracao() {
+  return (
+    <div className="integracao01">
+      <div className="vermelha">Texto 01</div>
+      <div className="azul">Texto 02</div>
+      <div className="branco">Texto 03</div>
+    </div>
+  );
+}
