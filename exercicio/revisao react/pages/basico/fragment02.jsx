9d44eb905e795f66746b7aa8ae7e2a877905efd1
Yuri Cunha <isyuricunha@icloud.com>
2022-06-25 11:26:48 -0300

    Create fragment02.jsx

---
+++ exercicio/revisao react/pages/basico/fragment02.jsx
@@ -0,0 +1,8 @@
+export default function fragment() {
+  return (
+    <> // isso importa o fragment, sem a necessidade de improtar o react from 'react'
+      <h1>Titulo</h1>
+      <h2>Subtitulo</h2>
+    </>
+  );
+}
